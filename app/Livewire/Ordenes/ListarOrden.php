5636e7377a2f745f6b5c7391a94a3b55a2020c58
Erickmela <2021110411@udh.edu.pe>
2024-10-23 22:09:05 -0500

    CRUD Orden

---
+++ app/Livewire/Ordenes/ListarOrden.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Livewire\Ordenes;
+
+use Livewire\Component;
+use App\Models\Ordenes;
+
+class ListarOrden extends Component
+{
+
+    public $ordenes;
+
+    protected $listeners = [
+        'guardado' => 'getOrdenes',
+        'ordenActualizada' => 'getOrdenes',
+        'ordenEliminada' => 'getOrdenes',
+    ];
+
+    public function mount()
+    {
+        $this->getOrdenes();
+    }
+
+    public function getOrdenes()
+    {
+        $this->ordenes = Ordenes::where('ord_estado', 1)
+        ->join('users', 'ordenes.mesero_id', '=', 'users.id')
+        ->join('mesas', 'ordenes.mesa_id', '=', 'mesas.id')
+        ->select('ordenes.*', 'users.name as usuario', 'mesas.numero as numero_mesa')
+        ->get();
+    }
+
+    public function eliminar($id)
+    {
+        $this->dispatch('eliminar', $id);
+    }
+
+    public function editar($id)
+    {
+        $this->dispatch('editar', $id);
+    }
+
+
+    public function render()
+    {
+        return view('livewire.ordenes.listar-orden', [
+            'ordenes' => $this->ordenes,
+
+        ]);
+    }
+}
--- resources/views/ordenes/index.blade.php
+++ resources/views/ordenes/index.blade.php
@@ -4,4 +4,19 @@
             Ordenes
         </h2>
     </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
+                <div class="flex justify-between items-center mb-4">
+                    @livewire('ordenes.crear-orden')
+                </div>
+                @livewire('ordenes.listar-orden')
+            </div>
+        </div>
+    </div>  
+
+    @livewire('ordenes.editar-orden')
+    @livewire('ordenes.eliminar-orden')
+
 </x-app-layout>
