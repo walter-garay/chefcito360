30c60d26d2424933cca353ee1e70750f1310d961
David Garay <2021110349@udh.edu.pe>
2024-11-28 19:28:55 -0500

    implementacion tailwind cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+
 
         <!-- Styles -->
         @livewireStyles
